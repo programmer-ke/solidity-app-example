47b36f1779518ae8a827c7c960b354061c532e0f
Kenny Rachuonyo <krmboya@gmail.com>
2022-06-25 15:22:04 +0300

    added transaction and deployment script

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+  uint64 totalWaves;
+
   constructor () {
-    console.log("Let's print money!");
+    console.log("Let's print some money!");
+  }
+
+  function wave() public {
+    totalWaves += 1;
+    console.log("%s has waved", msg.sender);
+  }
+
+  function getTotalWaves () public view returns (uint64) {
+    console.log("We have <%d> total waves", totalWaves);
+    return totalWaves;
   }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,14 +1,29 @@
 const main = async () => {
+    // get wallet address of deployer (hardhat automatically captures it)
+    // also get wallet of another random person (hardhat's environment provides this?)
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     // compiles contract and places it in artifacts directory
     const WaveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-
     // Creates local ethereum network and deploys contract
     const waveContract = await WaveContractFactory.deploy();
-
     // await for completed deployment
     // Constract constructor runs in the process
     await waveContract.deployed();
+
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
